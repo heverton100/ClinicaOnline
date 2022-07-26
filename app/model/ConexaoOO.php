85aa73b1f40818179682a3bd9e8aad87b61aff34
Heverton Rodrigues Camargo <heverton.rc.100@gmail.com>
2022-07-26 14:45:44 -0300

    Update ConexaoOO.php

--- app/model/ConexaoOO.php
+++ app/model/ConexaoOO.php
@@ -4,11 +4,11 @@ class ConexaoOO {
 
   function connection()
   {
-    $servername = "bggac0murcgmafe7ntgd-mysql.services.clever-cloud.com";
-    $username = "ujlluk9853xfym67";
-    $password = "Wd5s56K6PRhdzsRF3FWk";
+    $servername = "";
+    $username = "";
+    $password = "";
 
-    $banco = "bggac0murcgmafe7ntgd";
+    $banco = "";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password,$banco);
